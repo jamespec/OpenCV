b464ed86b560efbe664639fba620a6d8db881cb7
marypec <40308799+marypec@users.noreply.github.com>
2024-02-26 19:37:10 -0500

    Playing with OpenCV in Java

--- src/main/java/TestLinePipeLine.java
+++ src/main/java/TestLinePipeLine.java
@@ -1,7 +1,6 @@
-import org.opencv.core.*;
 import org.opencv.core.Point;
+import org.opencv.core.*;
 import org.opencv.imgproc.Imgproc;
-import org.opencv.imgproc.Moments;
 import org.opencv.videoio.VideoCapture;
 
 import javax.swing.*;
@@ -19,14 +18,22 @@ class TestLinePipeLine {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     }
 
-    private final static Scalar minGreen = new Scalar(24, 44, 57, 0);
-    private final static Scalar maxGreen = new Scalar(71, 159, 161, 255);
-    private final static Scalar minYellow = new Scalar(13, 99, 126, 0);
-    private final static Scalar maxYellow = new Scalar(25, 255, 233, 255);
+//    private final static Scalar minGreen = new Scalar(24, 44, 57, 0);
+//    private final static Scalar maxGreen = new Scalar(71, 159, 161, 255);
+//    private final static Scalar minYellow = new Scalar(13, 99, 126, 0);
+//    private final static Scalar maxYellow = new Scalar(25, 255, 233, 255);
+//    private final static Scalar minWhite = new Scalar(0, 0, 180, 0);
+//    private final static Scalar maxWhite = new Scalar(180, 27, 255, 255);
+//    private final static Scalar minPurple = new Scalar(131, 28, 85, 0);
+//    private final static Scalar maxPurple = new Scalar(157, 144, 216, 255);
+    private final static Scalar minGreen = new Scalar(36, 0, 48, 0);
+    private final static Scalar maxGreen = new Scalar(79, 153, 255, 255);
+    private final static Scalar minYellow = new Scalar(13, 112, 158, 0);
+    private final static Scalar maxYellow = new Scalar(25, 255, 255, 255);
     private final static Scalar minWhite = new Scalar(0, 0, 180, 0);
     private final static Scalar maxWhite = new Scalar(180, 27, 255, 255);
-    private final static Scalar minPurple = new Scalar(131, 28, 85, 0);
-    private final static Scalar maxPurple = new Scalar(157, 144, 216, 255);
+    private final static Scalar minPurple = new Scalar(131, 28, 71, 0);
+    private final static Scalar maxPurple = new Scalar(157, 116, 255, 255);
 
     private static BufferedImage Mat2BufferedImage(Mat m) {
         // Fastest code
@@ -68,8 +75,7 @@ class TestLinePipeLine {
         image.setIcon(icon);
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         VideoCapture capture = new VideoCapture(0, CAP_DSHOW);
 
         Mat mat = new Mat();
@@ -78,100 +84,60 @@ class TestLinePipeLine {
 
         capture.read(mat);
         Imgproc.cvtColor(mat, hsvMat, Imgproc.COLOR_BGR2HSV);
-        inRange(hsvMat, new Scalar(160.0,160.0,0.0,0.0), new Scalar(180.0,255.0,255.0,255.0), hsvMat);
         BufferedImage bi1 = Mat2BufferedImage(hsvMat);
         CreateImageFrame(bi1, "Original");
 
         List<MatOfPoint> contours = new ArrayList<>();
-        List<RotatedRect> rotatedRects = new ArrayList<>();
         Point center = new Point();
         float[] radius = new float[2];
         MatOfPoint2f mat2f = new MatOfPoint2f();
 
-        while(true) {
+        while (true) {
             capture.read(mat);
             Imgproc.cvtColor(mat, hsvMat, Imgproc.COLOR_BGR2HSV);
+
+            blur(hsvMat, 4.0, hsvMat);
+
             inRange(hsvMat, minGreen, maxGreen, colorMat);
-
-            LinePipeline.findContours(colorMat, true, contours);
-            for(int i=0; i<contours.size(); ++i ) {
-                MatOfPoint contour = contours.get(i);
-                contour.convertTo(mat2f, CvType.CV_32F);
-                Imgproc.minEnclosingCircle(mat2f, center, radius);
-
-                if( radius[0] > 20 ) {
-                    // draw the contour and center of the shape on the image
-                    Imgproc.drawContours(mat, contours, i, new Scalar(0, 255, 0), 2);
-                    Imgproc.putText(mat, "G", center, Core.FONT_HERSHEY_PLAIN, 1.0, new Scalar(255, 255, 255), 1);
-                }
-            }
+            Mat totalColorMat = colorMat.clone();
 
             inRange(hsvMat, minYellow, maxYellow, colorMat);
-
-            LinePipeline.findContours(colorMat, true, contours);
-            for(int i=0; i<contours.size(); ++i ) {
-                MatOfPoint contour = contours.get(i);
-                contour.convertTo(mat2f, CvType.CV_32F);
-                Imgproc.minEnclosingCircle(mat2f, center, radius);
-
-                if( radius[0] > 20 ) {
-                    // draw the contour and center of the shape on the image
-                    Imgproc.drawContours(mat, contours, i, new Scalar(0, 255, 0), 2);
-                    Imgproc.putText(mat, "Y", center, Core.FONT_HERSHEY_PLAIN, 1.0, new Scalar(255, 255, 255), 1);
-                }
-            }
-
+            Core.add(totalColorMat, colorMat, totalColorMat);
             inRange(hsvMat, minWhite, maxWhite, colorMat);
-
-            LinePipeline.findContours(colorMat, true, contours);
-            for(int i=0; i<contours.size(); ++i ) {
-                MatOfPoint contour = contours.get(i);
-                contour.convertTo(mat2f, CvType.CV_32F);
-                Imgproc.minEnclosingCircle(mat2f, center, radius);
-
-                if( radius[0] > 20 ) {
-                    // draw the contour and center of the shape on the image
-                    Imgproc.drawContours(mat, contours, i, new Scalar(0, 255, 0), 2);
-                    Imgproc.putText(mat, "W", center, Core.FONT_HERSHEY_PLAIN, 1.0, new Scalar(255, 255, 255), 1);
-                }
-            }
-
+            Core.add(totalColorMat, colorMat, totalColorMat);
             inRange(hsvMat, minPurple, maxPurple, colorMat);
+            Core.add(totalColorMat, colorMat, totalColorMat);
 
-            LinePipeline.findContours(colorMat, true, contours);
-            for(int i=0; i<contours.size(); ++i ) {
+            int contourCount = 0;
+            LinePipeline.findContours(totalColorMat, true, contours);
+            for (int i = 0; i < contours.size(); ++i) {
                 MatOfPoint contour = contours.get(i);
                 contour.convertTo(mat2f, CvType.CV_32F);
                 Imgproc.minEnclosingCircle(mat2f, center, radius);
 
-                if( radius[0] > 20 ) {
+                if (radius[0] > 15 && center.y > mat.height()/2.0) {
+                    contourCount++;
                     // draw the contour and center of the shape on the image
                     Imgproc.drawContours(mat, contours, i, new Scalar(0, 255, 0), 2);
-                    Imgproc.putText(mat, "P", center, Core.FONT_HERSHEY_PLAIN, 1.0, new Scalar(255, 255, 255), 1);
+
+                    String s = String.format("(%d, %d)", (int) center.x, (int) center.y);
+                    center.x += 30.0;
+                    Imgproc.putText(mat, s, center, Core.FONT_HERSHEY_PLAIN, 1.0, new Scalar(255, 255, 255), 1);
                 }
             }
+            String s = String.format("Contours: %d", contourCount );
+            Imgproc.putText(mat, s, new Point(100,100), Core.FONT_HERSHEY_PLAIN, 1.0, new Scalar(255, 255, 255), 1);
 
             bi1 = Mat2BufferedImage(mat);
             UpdateImage(bi1);
         }
+    }
 
-//            Imgproc.drawContours(mat, contours, -1, new Scalar(0,255,0), 3 );
-//            Imgproc.circle(mat, center, (int)radius[0], new Scalar(255, 255, 255), 2);
-//         double fontScale = (mat.width() > 352 ? 1.0 : 0.7);
-//        Mat contourImg = new Mat(logo.size(), logo.type(), black);
-//
-//        ArrayList<RotatedRect> rects = tp.findRotatedRectsOutput();
-//        for (RotatedRect r : rects) {
-//            Point[] vertices = new Point[4];
-//            r.points(vertices);
-//            for (int j = 0; j < 4; j++)
-//                Imgproc.line(contourImg, vertices[j], vertices[(j + 1) % 4], red);
-//
-//            double angle = Math.floor((r.size.width < r.size.height) ? r.angle + 90 : r.angle);
-//            Imgproc.putText(contourImg, "/" + angle, new Point(r.center.x, r.center.y), Core.FONT_HERSHEY_PLAIN, 0.75, white);
-//        }
+    private static void blur(Mat input, double doubleRadius, Mat output) {
+        int radius = (int) (doubleRadius + 0.5);
+        int kernelSize;
 
-//        BufferedImage bi5 = Mat2BufferedImage(contourImg);
-//        displayImage(bi1, "Contours");
+        kernelSize = 2 * radius + 1;
+        Imgproc.blur(input, output, new Size(kernelSize, kernelSize));
     }
 }
